9f3a40d2d3d73467f11cb773d2d1d9c4a2184ff6
Ben (Desktop/Arch) <bennyschulenburg@gmx.de>
2026-03-30 19:09:29 +0200

    Removed control plane that's not necessary yet

--- crates/control-plane/src/routes/mod.rs
+++
@@ -1,5 +0,0 @@
-mod relays;
-mod root;
-
-pub(crate) use relays::list_relays_get;
-pub(crate) use root::index_get;
--- crates/control-plane/src/routes/root.rs
+++
@@ -1,8 +0,0 @@
-use axum::{extract::State, response::Html};
-
-use crate::{ServerState, utils::response_html};
-
-pub(crate) async fn index_get(State(_state): State<ServerState>) -> Html<String> {
-    let html = "<h1>Welcome to the Hoshi control plane!</h1>";
-    response_html(html, "Hoshi Control Plane")
-}
